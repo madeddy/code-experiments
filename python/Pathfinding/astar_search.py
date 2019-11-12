48ed4bdc3fdef6e51e1b73adfdd1ffc1650c2ab1
madeddy <mad-eddy@gmx.de>
2019-11-12 02:40:23 +0100

    Switch to pathlib and minor changes; rename file

--- python/Pathfinding/astar_search.py
+++ python/Pathfinding/astar_search.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-'''Test.'''
+'''A* search implemenation over a previously constructed graph of the
+ walkable area of a map.'''
 
 # pylint: disable=w0511, C0103, C0301, r1710
 
-import os
+from pathlib import Path
 import argparse
 import pickle
 from heapq import heappop, heappush
-import numpy as np
-from math import sqrt
+# from math import sqrt
+import time
 from graphdata.gridsize import gridsize
 
+__title__ = 'astar_search'
+__license__ = 'MIT'
+__author__ = 'madeddy'
+__status__ = 'Development'
+__version__ = '0.14.0-alpha'
 
 # def heuristic(cell, goal):
 #     return abs(cell[0] - goal[0]) + abs(cell[1] - goal[1])
@@ -36,42 +42,52 @@ from graphdata.gridsize import gridsize
 #     visited = set()
 #
 #     while pr_queue:
-#         _, cost, path, current = heappop(pr_queue)
+#         _, cost, pathway, current = heappop(pr_queue)
 #         print(f'Heuristic_cost: {_} cost: {cost}')
 #         if current == goal:
-#             # print('A * solution return: ', path)
-#             # # return path
-#             save_path(path)
+#             # print('A * solution return: ', pathway)
+#             # # return pathway
+#             save_path(pathway)
 #         if current in visited:
 #             continue
 #         visited.add(current)
 #
 #         for neighbour, dir in graphdict[current]:
 #             heappush(pr_queue, (cost + heuristic(neighbour, goal),
-#                                 cost + 1, f'{path} {dir}', neighbour))
+#                                 cost + 1, f'{pathway} {dir}', neighbour))
 #
-#     return 'ERROR: No path to goal found!'
+#     return 'ERROR: No pathway to goal found!'
 
-class GraphPathSearch(object):
-    '''Class for all graph building related functionality.'''
+class GraphPathSearch:
+    '''Class for all graph search related functionality.'''
 
     def __init__(self, graphdict, start, goal):
         self.graph = graphdict
         self.start = start
         self.goal = goal
         self.explored = {}
-        self.path = []
+        self.pathway = []
 
         # self.x_sg = self.start[0] - self.goal[0]
         # self.y_sg = self.start[1] - self.goal[1]
 
+    def reconstruct_path(self):
+        '''This reconstructs the pathway from the explored list of the
+           A* search and returns it.'''
+
+        cur_node = self.goal
+        while cur_node != self.start:
+            self.pathway.append(cur_node)
+            cur_node = self.explored[cur_node]
+        self.pathway.append(self.start)  # optional
+        self.pathway.reverse()  # optional
+
     def heuristic(self, source, target):
         '''
-        Heuristic func for 8-direction movement.
-        The formula for diagonal distance:
-        orth * (dx + dy) + (diag - 2 * orth) * min(dx, dy)
-        orth = 1
+        Heuristic func for 8-direction movement. The formula for diagonal
+        distance: orth * (dx + dy) + (diag - 2 * orth) * min(dx, dy)
         diag = sqrt(2) - 2
+        orth = 1
         Our orthogonal move cost is 1; multiply with 1 is pointless
         '''
         diag = -0.5857864376269049
@@ -110,37 +126,26 @@ class GraphPathSearch(object):
                     self.explored[nxt_node] = current
 
         self.reconstruct_path()
-        return self.path, cur_cost
-
-    def reconstruct_path(self):
-        '''
-        This reconstructs the path from the explored list of the a_star_search
-        and returns it.
-        '''
-        cur_node = self.goal
-        while cur_node != self.start:
-            self.path.append(cur_node)
-            cur_node = self.explored[cur_node]
-        self.path.append(self.start)  # optional
-        self.path.reverse()  # optional
+        return self.pathway, cur_cost
 
 
-def save_path(path):
-    '''This writes the found path to a file.'''
+def save_path(pathway):
+    '''This writes the found pathway to a file.'''
     with open('graphdata/pathsearch_sol.txt', 'w') as nfi:
-        print('A* solution return: ', path, file=nfi)
+        print('A* solution return: ', pathway, file=nfi)
 
 
 def get_args():
     '''This is the parser function'''
+
     def valid_file(infilename):
         '''Help function to test the given string for the infilename.'''
-        if os.path.isfile(infilename):
-            if not infilename.endswith('.pickle'):
-                raise parser.error('Input must be a pickled file.')
-            return infilename
-        else:
+
+        if not Path(infilename).is_file():
             raise parser.error('Input file not found.')
+        if Path(infilename).suffix not in '.pickle':
+            raise parser.error('Input must be a pickled file.')
+        return infilename
 
     parser = argparse.ArgumentParser(description='Finds path from a source point to a target in the stored graph.')
     parser.add_argument('graph_file', action='store', type=valid_file,
@@ -150,26 +155,17 @@ def get_args():
 
 
 def main(cfg):
-    '''main func'''
-    # array spacesaving in np file; re-usable
-    # with open('control/maskarray.npy', 'rb') as nfi:
-    #     mask_array = np.load(nfi)
-    # graphdict = dd.io.load(cfg.graph_file)
+    '''...main function.'''
+
     with open(cfg.graph_file, 'rb') as nfi:
         graphdict = pickle.load(nfi)
 
     # pathfinding
-    # find_path_bfs(mask_array, graphdict)
-    # find_path_astar(mask_array, graphdict)
-
-    import time
     t = time.process_time()
     start, goal = (1, 1), (gridsize[0] - 2, gridsize[1] - 2)
     gps = GraphPathSearch(graphdict, start, goal)
 
     found_path, cost = gps.a_star_pathsearch()
-
-    # found_path = reconstruct_path(explored, start, goal)
 
     print(time.process_time() - t)
 
@@ -179,3 +175,4 @@ def main(cfg):
 
 if __name__ == '__main__':
     main(get_args())
+
